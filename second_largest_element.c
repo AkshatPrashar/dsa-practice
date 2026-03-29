6beeef10072ccd6ea7d23cad6d98b3197bb3fc19
AkshatPrashar <akshat.prashar104@gmail.com>
2026-03-29 12:15:09 +0530

    Implement function to find second largest element
    Approach
    Traverse the array once.
    Maintain two variables:
    largest → stores the maximum element found so far.
    slargest → stores the second largest element.
    Update values dynamically:
    If a new maximum is found → update both largest and slargest.
    Otherwise, update slargest only if the current element lies between them.
     Time & Space Complexity
    Time Complexity: O(n) (single traversal)
    Space Complexity: O(1) (no extra space used)
     Edge Case
    If all elements are equal (like {6,6,6,6}), the function returns -1, indicating that a second largest element does not exist.
     Key Idea
    
    Efficiently track the top two distinct values without sorting the array, making the solution optimal for large inputs.

---
+++ second_largest_element.c
@@ -0,0 +1,25 @@
+int find_sec_largest(vector<int>& arr,int n){
+    int largest=arr[0];
+    int slargest=-1;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>largest){
+            slargest=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]<largest && arr[i]>slargest){
+            slargest=arr[i];
+        }
+    }
+
+    return slargest;
+}
+
+int main(){
+    vector<int> arr={6,6,6,6,6,6};
+
+    int s_largest_ele=find_sec_largest(arr,arr.size());
+
+    cout<<"Second largest: "<<s_largest_ele<<"\n";
+
+}
